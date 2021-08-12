89644a9623a552fc0743e31396982e0ccd136b8c
Ryan Emmans <ryanemmans@gmail.com>
2021-08-11 21:34:10 -0700

    Fixed Step 10, Part F

--- app.js
+++ app.js
@@ -156,7 +156,9 @@ let joe = new Student('Joe', 'Schmoe', 100);
 
 
 
-Student.prototype.greeting = () => `Hi, my name is ${this.name}`;
+Student.prototype.greeting = function() {
+  return `Hi, my name is ${this.name}`;
+};
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this method to break!
